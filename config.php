0edaa40129604b60aac12ed887ade9b9861df8fe
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-17 15:22:55 +0700

    add database config file

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_SERVER','111.223.52.146');
+    define('DB_USERNAME','zp11276_airlines');
+    define('DB_PASSWORD','FrameMarkKieSine123');
+    define('DB_DATABASE','zp11276_airlines');
+?>
