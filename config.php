db9aa61f93fe2d3a83d5c4ac593c607f39175350
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-17 20:50:28 +0700

    add error handling

--- config.php
+++ config.php
@@ -4,4 +4,7 @@
     define('DB_PASSWORD','FrameMarkKieSine123'); #Database password
     define('DB_DATABASE','zp11276_airline'); #Database name
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE); #Database connection
+    if (mysqli_connect_errno($db)) { #when error occur
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } 
 ?>
