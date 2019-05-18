a7505eb0d6625cb5fba32ae13791a530436bab7d
pcybxine <sine.narisa@gmail.com>
2019-05-19 01:29:54 +0700

    Passenger Detail

---
+++ PassengerDetail.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <h1>Passenger Detail</h1>
+        <form>
+            <p>Adult 1</p>
+            <hr>
+            <br>
+            <input type="text" name="FirstName" placeholder="First Name">
+            <input type="text" name="LastName" placeholder="Last Name"><br><br>
+            <p>Dath of Birth :</p>
+            <input type="Date" name="DOB">
+            <input type="radio" name="sex" value="m"checked>Male
+            <input type="radio" name="sex" value="f">Female<br><br>
+            <input type="submit" value="Next">
+        </form>
+    </body>
+</html>
