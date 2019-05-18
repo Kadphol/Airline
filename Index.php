1ed85c8e35106abc3a846260b8a902b50d34de73
pcybxine <sine.narisa@gmail.com>
2019-05-19 01:33:13 +0700

    Index: DropDown list not finished, can't add (count+)

---
+++ Index.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+        <form>
+            <input type="text" name="Departure" placeholder="Departure Airport">
+            <input type="text" name="Arrival" placeholder="Arrival Airport">
+            <p>Departure : &emsp;&emsp;&emsp;&emsp;Returning :</p>
+            <input type="Date" nam="DepartureDate">
+            <input type="Date" nam="Returning"><br><br>
+            <select name="Passenger">
+                <option value="" Selected>--Selected Type--</option>
+                <option value="Adult">Adult</option>
+                <option value="Child">Child</option>
+                <option value="Infant">Infant</option> 
+            </select>
+            <select name="Class">
+                <option value="" Selected>--Selected Class--</option>
+                <option value="FirsstClass" >Firsst Class</option>
+                <option value="Business">Business</option>
+                <option value="Economy">Economy</option>
+            </select><br><br>
+            <input type="submit" value="Search Flight">
+        </form>
+    </body>
+</html>
