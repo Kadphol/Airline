40ec651f0d808be87775647224bb542f19d08f7f
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-18 22:58:41 +0700

    add staff login page & transfer config to admin

---
+++ admin/config/config.php
@@ -0,0 +1,10 @@
+<?php #Database configuration file
+    define('DB_SERVER','localhost'); #Database server name
+    define('DB_USERNAME','zp11276_airlines'); #Database username
+    define('DB_PASSWORD','FrameMarkKieSine123'); #Database password
+    define('DB_DATABASE','zp11276_airline'); #Database name
+    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE); #Database connection
+    if (mysqli_connect_errno($db)) { #when error occur
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } 
+?>
