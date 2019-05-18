c60d0396ca6de0974caf55c2522293a59d5b5699
pcybxine <sine.narisa@gmail.com>
2019-05-19 01:27:30 +0700

    Register Form

---
+++ RegisForm.php
@@ -0,0 +1,25 @@
+<html>
+    <body>
+        <form>
+            <h1>Regis Form</h1>
+            <input type="email" name="Email" placeholder="Email"> <br><br>
+            <input type="password" name="Password" placeholder="Password"><br><br>
+            <input type="password" name="ConPassword" placeholder="Confirm Password"><br><br>
+            <input type="text" name="Passport" placeholder="Passport No."><br><br>
+            <input type="text" name="FirstName" placeholder="First Name">
+            <input type="text" name="LastName" placeholder="Last Name"><br><br>
+            <input type="radio" name="sex" value="m"checked>Male
+            <input type="radio" name="sex" value="f">Female<br><br>
+            <p>Dath of Birth :</p>
+            <input type="Date" name="DOB" value="DOB"><br><br>
+            <select name="Country">
+                <option value="" Selected>--Country--</option>
+                <option value="+66">+66</option>
+                <option value="+81">+81</option>
+                <option value="+48">+48</option>
+            </select>
+            <input type="tel" name="PhoneNumber" placeholder="Phone Number"><br><br>
+            <input type="submit" value="Sign Up">
+        </form>
+    </body>
+</html>
