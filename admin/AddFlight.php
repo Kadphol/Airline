9a0442493e49532e57942bde00176669c76d5267
pcybxine <sine.narisa@gmail.com>
2019-05-19 01:15:13 +0700

    Add Flight Form

---
+++ admin/AddFlight.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+</head>
+
+<body>
+    <h1>Add Flight</h1>
+    <p>
+        AirportID <input type="text" name="AirportID">
+        Airport Name <input type="text" name="AirportName"><br><br>
+        RouteID <input type="text" name="RouteID">
+        From<input type="text" name="Departure"> to <input type="text" name="Arrival">
+    </p>
+    <hr>
+    <p>
+        FlightID <input type="text" name="FlightID">
+        Duration <input type="text" name="Duration"><br><br>
+        Departure Time <input type="text" name="DepartureTime">
+        Arrival Time <input type="text" name="ArrivalTime"><br><br>
+        AirplanID <input type="text" name="AirplaneID">
+        Gate <input type="text" name="Gate">
+    </p>
+    <hr>
+    <h2>Day of Oparation</h2>
+    <p>
+        StartOparation <input type="text" name="StartOparation">
+        EndOparation <input type="text" name="EndOparation"><br><br>
+        Day of Oparation <input type="text" name="DOP">
+        Status <input type="text" name="Status">
+    </p>
+    <input type="submit" value="Add Flight">
+</body>
+</html>
