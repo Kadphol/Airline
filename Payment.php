d94aa9dd47a8b4a3bb56c0e8d77d7ea6c9f24333
pcybxine <sine.narisa@gmail.com>
2019-05-19 01:31:15 +0700

    Payment

---
+++ Payment.php
@@ -0,0 +1,72 @@
+<html>
+
+<body>
+    <h1>Contact Details</h1>
+    <form>
+        <input type="text" name="FirstName" placeholder="First Name">
+        <input type="text" name="LastName" placeholder="Last Name"><br><br>
+        <input type="email" name="Email" placeholder="Email"><br><br>
+        <input type="tel" name="PhoneNumber" placeholder="Phone Number">
+    </form>
+
+    <h1>Booking Details</h1>
+    <p>&emsp;Depart Date<br>
+        &emsp;11 June<br>
+        &emsp;Bangkok-Don Mueng -> Phitsanulok<br>
+        &emsp;FD 3304 / 12:05-13.05 / 1h 0m</p>
+
+    <h1>Payment</h1>
+    
+    <form>
+        <p>Credit/Debit Card</p>
+        <input type="text" name="Card" placeholder="Card Number"><br><br>
+        <select name='Month'>
+            <option value='' Selected>--Select Month--</option>
+            <option value='1'>Janaury</option>
+            <option value='2'>February</option>
+            <option value='3'>March</option>
+            <option value='4'>April</option>
+            <option value='5'>May</option>
+            <option value='6'>June</option>
+            <option value='7'>July</option>
+            <option value='8'>August</option>
+            <option value='9'>September</option>
+            <option value='10'>October</option>
+            <option value='11'>November</option>
+            <option value='12'>December</option>
+        </select>
+        <select name="Year">
+            <option value='' Selected>--Select Year--</option>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+            <option value="2014">2014</option>
+            <option value="2013">2013</option>
+            <option value="2012">2012</option>
+            <option value="2011">2011</option>
+            <option value="2010">2010</option>
+            <option value="2009">2009</option>
+            <option value="2008">2008</option>
+            <option value="2007">2007</option>
+            <option value="2006">2006</option>
+            <option value="2005">2005</option>
+            <option value="2004">2004</option>
+            <option value="2003">2003</option>
+            <option value="2002">2002</option>
+            <option value="2001">2001</option>
+            <option value="2000">2000</option>
+            <option value="1999">1999</option>
+            <option value="1999">1999</option>
+            <option value="1999">1999</option>
+            <option value="1999">1998</option>
+            <option value="1999">1997</option>
+            <option value="1999">1996</option>
+        </select>
+        <input type="text" name="CVV" placeholder="CVV"><br><br>
+        <input type="text" name="Name" placeholder="Name On Card"><br><br>
+        <input type="submit" value="Search Flight">
+    </form>
+</body>
+
+</html>
