e9409b026fc59a5b397ed972c0a14613c1724246
pcybxine <sine.narisa@gmail.com>
2019-05-21 20:40:21 +0700

    Confirm Booking stupid form

---
+++ ConfirmBooking.php
@@ -0,0 +1,25 @@
+<html>
+
+<body>
+    <h2>Payment Success!</h2>
+    <p>Billing ID : B002</p>
+    <p>Booking ID : BB12090</p>
+    <br>
+    <h1>Billing ID : B002</h1>
+    <p>
+        &emsp;Depart Date<br>
+        &emsp;11 June<br><br>
+        &emsp;Bangkok-Don Mueng -> Phitsanulok<br>
+        &emsp;FD 3304 / 12:05-13.05 / 1h 0m
+    </p>
+    <br>
+    <h1>Booking ID : BB12090</h1>
+    <form>
+        <input type="text" name="FirstName" placeholder="First Name">
+        <input type="text" name="LastName" placeholder="Last Name"><br><br>
+        <input type="email" name="Email" placeholder="Email">
+        <input type="tel" name="PhoneNumber" placeholder="Phone Number">
+    </form>
+</body>
+
+</html>
