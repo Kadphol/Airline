d9b79ea97db987b07ba21e915d3e670faeef6b93
pcybxine <sine.narisa@gmail.com>
2019-05-22 20:44:39 +0700

    WelcomeStaff From: Not Linked

---
+++ admin/WelcomeStaff.php
@@ -0,0 +1,20 @@
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <h1>Welcome!</h1>
+    <hr>
+    <input type="button" value="Search Airport">
+    <input type="button" value="Manage Flight">
+    <input type="button" value="Manage Airport">
+    <input type="button" value="Manage Staff">
+    <input type="button" value="Staff Registration">
+    <input type="button" value="Analysis Form">
+
+</body>
+
+</html>
