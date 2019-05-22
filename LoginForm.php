1c244e046d548b4912b6b277a982549bf93434b1
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-22 23:41:47 +0700

    Add member login form

---
+++ LoginForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Member login</title>
+        <script   src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div align = "center">
+            <div style = "width:300px; border: solid 1px #333333;" align = "left">
+                <div tyle = "margin:30px">
+                    <form action = "" method = "post" id = "loginform">
+                        <label>Email :</label>
+                        <input type = "text" name = "email" id = "email"><br><br>
+                        <label>Password :</label>
+                        <input type = "text" name = "password" id = "password"><br><br>
+                        <input type = "submit" value = "login"><br><br>
+                        <a href = "RegisForm.php">Register</a><br><br>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
