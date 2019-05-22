f9634d6b5bb32c93c7803db5f74d2d510b9045f8
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-22 15:51:45 +0700

    Insert to DB test version

--- MemberRegis.php
+++ MemberRegis.php
@@ -2,40 +2,48 @@
     #include("admin/config/config.php");
     session_start();
     $errors = array();
-    $db = mysqli_connect("localhost","root","","airline");
-    echo "You have Successfully Registered.....";
+    $db = mysqli_connect("localhost","root","","Airline_test");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MYSQL: ".mysqli_connect_error();
+    } 
+    #echo "You have Successfully Registered.....";
     #echo $_POST["email"];
-    $email = mysqli_real_escape_string($db,$_POST["email"]);
-    $pwd = mysqli_real_escape_string($db,$_POST["password"]);
-    $passport = mysqli_real_escape_string($db,$_POST["passport"]);
-    $fname = mysqli_real_escape_string($db,$_POST["fname"]);
-    $lname = mysqli_real_escape_string($db,$_POST["lname"]);
+    $email = $_POST["Email"];
+    $pwd = mysqli_real_escape_string($db,$_POST["Password"]);
+    $passport = mysqli_real_escape_string($db,$_POST["Passport"]);
+    $fname = mysqli_real_escape_string($db,$_POST["FirstName"]);
+    $lname = mysqli_real_escape_string($db,$_POST["LastName"]);
     $sex = mysqli_real_escape_string($db,$_POST["sex"]);
     $DOB = mysqli_real_escape_string($db,$_POST["DOB"]);
-    $country = mysqli_real_escape_string($db,$_POST["country"]);
-    $phone = mysqli_real_escape_string($db,$_POST["phone"]);
+    $country = mysqli_real_escape_string($db,$_POST["Country"]);
+    $phone = mysqli_real_escape_string($db,$_POST["PhoneNumber"]);
 
-    $check_query = "SELECT * FROM Email WHERE Email = '$email' LIMIT 1";
+    $check_query = "SELECT Email FROM Member WHERE Email = '$email' LIMIT 1";
     $result = mysqli_query($db,$check_query);
     $user = mysqli_fetch_assoc($result);
 
     if ($user) { // if user exists
-        if ($user['email'] === $email) {
+        if ($user['Email'] === $email) {
           array_push($errors, "email already exists");
         }
     }
 
     if(count($errors)==0) {
-        $pwd = md5($pwd);
-
-        $query = "INSERT INTO Member (Email,Password,FirstName,LastName,Sex,DOB,Country,PhoneNumber,Passport)
-        VALUES($email,$pwd,$fname,$lname,$sex,$DOB,$country,$phone,$passport)";
+        #$pwd = md5($pwd);
+        if($sex == 'm') {
+            $prefix = "Mr.";
+        } else {
+            $prefix = "Mrs.";
+        }
+        $query = "INSERT INTO Member (Email,Password,Prefix,FirstName,LastName,DOB,Country,PhoneNumber,Passport,MilesPoint)
+        VALUES('$email',$pwd,'$prefix','$fname','$lname','$DOB','$country',$phone,'$passport',0)";
         if (!mysqli_query($db,$query)) {
-            die('Error: '.mysqli_error($con));
+            die('Error: '.mysqli_error($db));
         } else{
             $_SESSION['name'] = $fname;
             $_SESSION['success'] = "You are now loggin in";
-            header('location: index.php');
+            echo "success";
+            #header('location: index.php');
         }
     }
 
