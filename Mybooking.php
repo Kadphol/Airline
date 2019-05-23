3b86c2db3db1ee01d9add82e9ad878dda64f3be2
pcybxine <sine.narisa@gmail.com>
2019-05-23 16:54:47 +0700

    My Booking-Login

---
+++ Mybooking.php
@@ -0,0 +1,97 @@
+<html>
+
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>My Booking</h1>
+    <h2>Current Booking</h2>
+    <hr>
+    <table>
+        <tr>
+            <th>Departure Date</th>
+            <th>Origin</th>
+            <th>Destination</th>
+            <th>Booking No.</th>
+            <th>Guest Name</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+    </table>
+    <br>
+    <h2>Complete Booking</h2>
+    <hr>
+    <table>
+        <tr>
+            <th>Departure Date</th>
+            <th>Origin</th>
+            <th>Destination</th>
+            <th>Booking No.</th>
+            <th>Guest Name</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+        <tr>
+            <td>Tuesday, 11 June 2019</td>
+            <td>Don Mueng International Airport</td>
+            <td>Phitsanulok</td>
+            <td>238434</td>
+            <td>Hoseok Jung</td>
+            <td>View</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
