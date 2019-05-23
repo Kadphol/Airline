fcc00343125fabd123883513960086b360b0afdf
pcybxine <sine.narisa@gmail.com>
2019-05-24 01:30:08 +0700

    Staff Profile text

---
+++ admin/StaffProfile.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+        <h1>Staff Profile : S234234</h1>
+        <h3>Account</h3>
+        <hr>
+        <p>
+            Email<br>
+            &emsp;jungkookisgoldenmaknae@gmail.com
+        </p>
+        <br>
+        <h3>Profile</h3>
+        <hr>
+        <p>
+            Personal Info<br>
+            &emsp;Name<br>
+            &emsp;Jungkookie Jeon<br><br>
+            &emsp;Sex<br>
+            &emsp;Male<br><br>
+            &emsp;Passport No.<br>
+            &emsp;234234234234234<br><br>
+            &emsp;Street Address<br>
+            &emsp;3/100 The cube pracha Uthit Building B Soi Pracha Uthit37 Ratchaburana Bangkok 10140<br><br>
+        &emsp;Date of Birth<br>
+        &emsp;01/09/1999<br><br>
+        &emsp;Phone Number<br>
+        &emsp;(+66)345345345<br><br>
+        Staff Info<br>
+        &emsp;AirportID<br>
+        &emsp;CMX<br><br>
+        &emsp;StaffID<br>
+        &emsp;S234234<br><br>
+        &emsp;Position<br>
+        &emsp;Pilot
+        </p>
+    </body>
+</html>
