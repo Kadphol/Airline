28ca0a462afcde69b87b9030f89ca87be9754b19
Chonrada Kongmuang <chonradak@Chonradas-MacBook-Pro.local>
2019-05-24 01:21:38 +0700

    Update ConfirmBooking with CSS

--- ConfirmBooking.php
+++ ConfirmBooking.php
@@ -1,94 +1,187 @@
+
+<!-- import font roboto -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- import style -->
+<link rel="stylesheet" type="text/css" href="DefaultStyle.css">
+<link rel="stylesheet" type="text/css" href="AdditionStyle.css">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <html>
 
-<head>
-    <style>
-table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+    <head>
+        <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
 
-        td,
-        th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-        </style>
-</head>
+    <body>
+        <div class="card-container col-md-8">
+            <div class="card-body text-center">
+            <i class="far fa-check-circle fa-5x text-success"></i><br><br>
+                    <h2><strong>Payment Success!</strong></h2>
+                    <p>
+                        Billing ID : B002 <br>
+                        Booking ID : BB12090
+                    </p>
+            </div>
+        </div>
+        <br>
 
-<body>
-    <h2>Payment Success!</h2>
-    <p>Billing ID : B002<br>
-        Booking ID : BB12090
-    </p>
-    <br>
+        <div class="card-container col-md-8 mt-0">
+            <div class="card-body">
+                <h3><strong>Confirmation</strong></h3>
+                <hr>
+                <li class="list-group-item list-group-item-success"><i class="far fa-check-circle text-success"></i> Confirmed</li>
+            </div>
+        </div>
+        
+        <div class="card-container col-md-8 mt-4">
+            <div class="card-body">
+                <h3><b>Billing ID : B002</b></h3>
+                <hr>
+                <div class="row mt-3">
+                    <div class="col-md-6 border-right">
+                        <div class="container">
+                            <h4><i class="far fa-check-circle text-success"></i> <b>Payment Confirmed</b></h4>
+                            <p>
+                                <b>Contact person</b> : Kim Namjoon<br>
+                                <b>Email</b> : eiei@gmail.com
+                            </p>
+                            <h4><b>Flight</b></h4>
+                            <p>Subtotal 1x Guest&emsp;500.00 THB</p>
+                            <h4> <b>Add-ons&Fees</b></h4>
+                            <table style="width:70%">
+                                <tr>
+                                    <td>Check baggage </td>
+                                    <td></td>
+                                    <td>1003.90</td>
+                                    <td>THB</td>
+                                </tr>
+                                <tr>
+                                    <td>Counter Payment </td>
+                                    <td></td>
+                                    <td>74.90</td>
+                                    <td>THB</td>
+                                </tr>
+                                <tr>
+                                    <td>Subtotal</td>
+                                    <td></td>
+                                    <td>503.90</td>
+                                    <td>THB</td>
+                                </tr>
+                            </table>
+                            <br>
+                            <table style="width:70%">
+                                <tr>
+                                    <td>Total amount</td>
+                                    <td></td>
+                                    <td>1003.90</td>
+                                    <td>THB</td>
+                                </tr>
+                                <tr>
+                                    <td>Total VAT</td>
+                                    <td></td>
+                                    <td>26.17</td>
+                                    <td>THB</td>
+                                </tr>
+                                <tr>
+                                    <td>Total Paid</td>
+                                    <td></td>
+                                    <td>1003.90</td>
+                                    <td>THB</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <h4><b>Payment Date</b></h4>
+                        <table style="width:70%">
+                            <tr>
+                                <td>Date</td>
+                                <td>Tue,07 May 2019</td>
+                            </tr>
+                            <tr>
+                                <td>Type</td>
+                                <td>Credit Card</td>
+                            </tr>
+                            <tr>
+                                <td>Amount</td>
+                                <td>1003.93 THB</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <h2>Confirmation</h2>
-    <p>Confirmed</p>
-    <br>
+        <div class="card-container col-md-8 mt-4 bottom-margin">
+            <div class="card-body">
+            <h3><b>Booking ID : BB12090</b></h3>
+            <hr>
+            <h4><b>Flight 1 : Bangkok-Don Mueang Phitsanulok</b></h4>
+            <table class="table">
+                    <tr>
+                        <td style="background-color:#ededed;">
+                            <h4>FF3304</h4>
+                            <h4>Low Fare</h4>
+                        </td>
+                        <td> 
+                            <p>Bangkok-Don Mueang (DMK)<br>
+                            Don Mueang International Airport<br>
+                            Tue 11 Jun 2019<br>
+                            1205 hrs (1205PM)</p>
+                        </td>
+                        <td>
+                            <p>Phitsanulok(PHS) <br>
+                            Phitsanulok Airport<br>
+                            Tue 11 Jun 2019<br>
+                            1305 hrs (1:05PM)</p>
+                        </td>
+                    </tr>
+            </table>
+            </div>
+            <div class="ml-4 mt-0">
+                <h4><b>Guest Detail</b></h4>
+                <table style="width: 30%">
+                    <tr>
+                        <td>Adult 1 : </td>
+                        <td>Jeon Jungkook</td>
+                    </tr>
+                    <tr>
+                        <td>Nationality/Region : </td>
+                        <td>Thailand</td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth :</td>
+                        <td>31/10/1999</td>
+                    </tr>
+                </table>
+                <br>
+                <h4><b>Add-ons</b></h4>
+                <h4><b>DMK-PHS</b></h4>
 
-    <h1>Billing ID : B002</h1>
-    <h3>Payment Confirmed</h3>
-    <p>
-        Contact person : Kim Namjoon
-        Email : eiei@gmail.com
-    </p>
-    <h3>Flight</h3>
-    <p>Subtotal 1x Guest&emsp;500.00 THB</p>
-    <h3>Add-ons&Fees</h3>
-    <p>
-        Checked baggage&emsp;1003.90 THB<br>
-        Counter payment&emsp;74.90 THB<br>
-        Subtotal&emsp;&emsp;503.90 THB<br>
-    </p>
-    <p>
-        Total amoumt&emsp;1003.90 THB<br>
-        Total VAT&emsp;26.17 THB<br>
-        Total Paid&emsp;1003.90 THB<br>
-    </p>
-    <h3>Payment Date</h3>
-    <p>
-        Date&emsp;Tue,07 May 2019(UTC)<br>
-        Type&emsp;Credit Card<br>
-        Amount&emsp;1003.90 THB<br>
-    </p>
-    <br>
-
-    <h1>Booking ID : BB12090</h1>
-    <h3>Flight 1 : Bangkok-Don Mueang Phitsanulok</h3>
-    <table>
-        <tr>
-            <td>
-                <h4>FF3304</h4>
-                <h4>Low Fare</h4>
-            </td>
-            <td>
-                <p>Bangkok-Don Mueang(DMK)</p>
-                <p>Don Mueang International Airport</p>
-                <p>Tue 11 Jun 2019</p>
-                <p>1205 hrs(1205PM)</p>
-            </td>
-            <td>
-                <p>Phitsanulok(PHS)</p>
-                <p>Phitsanulok Airport</p>
-                <p>Tue 11 Jun 2019</p>
-                <p>1305 hrs(1:05PM)</p>
-            </td>
-        </tr>
-    </table>
-    <h3>Guest Detail</h3>
-    <p>
-        Adult 1 : Jeon Jongkook<br>
-        Nationality/Region : Thailand<br>
-        Date of Bird : 31/10/1999<br>
-    </p>
-    <h3>Add-ons</h3>
-    <h3>DMK-PHS</h3>
-    <p>
-        Auto Seat Assignment<br>
-        Checked baggage 20 kg<br>
-        SEAT 10F
-    </p>
-</body>
-
+                <p style="line-height: 15pt;">
+                    Auto Seat Assignment<br>
+                    Checked baggage 20 kg<br>
+                    SEAT 10F
+                </p>
+                <br>
+            </div>
+            </div>
+        </div>
+    </body>
 </html>
