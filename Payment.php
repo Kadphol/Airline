49878ad8570b5c95f911429ee57365276863aba0
Chonrada Kongmuang <chonradak@Chonradas-MacBook-Pro.local>
2019-05-23 16:01:58 +0700

    update payment with new css

--- Payment.php
+++ Payment.php
@@ -2,7 +2,7 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
 <!-- import style -->
-<link rel="stylesheet" type="text/css" href="StyleRegister.css">
+<link rel="stylesheet" type="text/css" href="RealStyle.css">
 
 <!-- font awesome -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
