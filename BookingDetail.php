140ec8441f15f627b540d71059f847504f647efd
pcybxine <sine.narisa@gmail.com>
2019-05-23 17:02:17 +0700

    Booking Detail-Login

---
+++ BookingDetail.php
@@ -0,0 +1,90 @@
+<html>
+
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Booking Detail</h1>
+    <h3>
+        Booking Number: DP3G4S<br>
+        Booking Date: 31 October 2020
+    </h3>
+    <h2>Flight Detail</h2>
+    <hr>
+    <p>Flight 1 : Bangkok - Don Mueng Phitsanulok</p>
+    <table>
+        <tr>
+            <td>
+                <h4>FF3304</h4>
+                <h4>Low Fare</h4>
+            </td>
+            <td>
+                <p>Bangkok-Don Mueang(DMK)</p>
+                <p>Don Mueang International Airport</p>
+                <p>Tue 11 Jun 2019</p>
+                <p>1205 hrs(1205PM)</p>
+            </td>
+            <td>
+                <p>Phitsanulok(PHS)</p>
+                <p>Phitsanulok Airport</p>
+                <p>Tue 11 Jun 2019</p>
+                <p>1305 hrs(1:05PM)</p>
+            </td>
+        </tr>
+    </table>
+    <br>
+
+    <h2>Guest Detail</h2>
+    <hr>
+    <table>
+        <tr>
+            <td>
+                <h4>Mr Jeon</h4>
+                <h4>Jongkook</h4>
+            </td>
+            <td>
+                <p>- Checked Baggage 20 kg</p>
+            </td>
+        </tr>
+    </table>
+
+    <h2>Payment Detail</h2>
+    <p>
+        Contact person : Kim Namjoon
+        Email : eiei@gmail.com
+    </p>
+    <h3>Flight</h3>
+    <p>Subtotal 1x Guest&emsp;500.00 THB</p>
+    <h3>Add-ons&Fees</h3>
+    <p>
+        Checked baggage&emsp;1003.90 THB<br>
+        Counter payment&emsp;74.90 THB<br>
+        Subtotal&emsp;&emsp;503.90 THB<br>
+    </p>
+    <p>
+        Total amoumt&emsp;1003.90 THB<br>
+        Total VAT&emsp;26.17 THB<br>
+        Total Paid&emsp;1003.90 THB<br>
+    </p>
+    <h3>Payment Date</h3>
+    <p>
+        Date&emsp;Tue,07 May 2019(UTC)<br>
+        Type&emsp;Credit Card<br>
+        Amount&emsp;1003.90 THB<br>
+    </p>
+</body>
+
+</html>
