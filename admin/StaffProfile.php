ea6bee66e1d124b2f02d070f2f5553b5070a62cb
pcybxine <sine.narisa@gmail.com>
2019-05-25 20:25:46 +0700

    Staff Profile Update

--- admin/StaffProfile.php
+++ admin/StaffProfile.php
@@ -1,11 +1,35 @@
+<?php
+    session_start();
+    #include("config/config.php");
+    $db = mysqli_connect("localhost", "root", "", "airline");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    $query = mysqli_query($db, "SELECT * FROM staff WHERE StaffID = '".$_SESSION['StaffID']."'");
+    while ($result = mysqli_fetch_array($query)) {
+        $StaffID = $result['StaffID'];
+        $Email = $result['Email'];
+        $Firstname = $result['FirstName'];
+        $Lastname = $result['LastName'];
+        $Sex = $result['Sex'];
+        $Passport = $result['Passport'];
+        $Address = $result['Address'];
+        $DOB = $result['DOB'];
+        $Country = $result['Country'];
+        $Phone = $result['PhoneNumber'];
+        $AirportID = $result['AirportID'];
+        $Position = $result['Position'];
+    }   
+?>
+
 <html>
     <body>
-        <h1>Staff Profile : S234234</h1>
+        <h1>Staff Profile : <?php echo $StaffID?></h1>
         <h3>Account</h3>
         <hr>
         <p>
             Email<br>
-            &emsp;jungkookisgoldenmaknae@gmail.com
+            &emsp;<?php echo $Email?>
         </p>
         <br>
         <h3>Profile</h3>
@@ -13,24 +37,25 @@
         <p>
             Personal Info<br>
             &emsp;Name<br>
-            &emsp;Jungkookie Jeon<br><br>
+            &emsp;<?php echo $Firstname." ".$Lastname?><br><br>
             &emsp;Sex<br>
-            &emsp;Male<br><br>
+            &emsp;<?php if($Sex=='m'){echo "Male";}
+                        else{echo "Female";} ?><br><br>
             &emsp;Passport No.<br>
-            &emsp;234234234234234<br><br>
+            &emsp;<?php echo $Passport?><br><br>
             &emsp;Street Address<br>
-            &emsp;3/100 The cube pracha Uthit Building B Soi Pracha Uthit37 Ratchaburana Bangkok 10140<br><br>
+            &emsp;<?php echo $Address?><br><br>
         &emsp;Date of Birth<br>
-        &emsp;01/09/1999<br><br>
+        &emsp;<?php echo $DOB?><br><br>
         &emsp;Phone Number<br>
-        &emsp;(+66)345345345<br><br>
+        &emsp;<?php echo "(".$Country.")".$Phone?><br><br>
         Staff Info<br>
         &emsp;AirportID<br>
-        &emsp;CMX<br><br>
+        &emsp;<?php echo $AirportID?><br><br>
         &emsp;StaffID<br>
-        &emsp;S234234<br><br>
+        &emsp;<?php echo $StaffID?><br><br>
         &emsp;Position<br>
-        &emsp;Pilot
+        &emsp;<?php echo $Position?>
         </p>
     </body>
 </html>
