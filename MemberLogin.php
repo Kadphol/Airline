d7febc0eb1ddc0c5d54c049c0032b2542f44f341
Kadphol <kadphol.len@mail.kmutt.ac.th>
2019-05-26 10:46:30 +0700

    Add session to member login

--- MemberLogin.php
+++ MemberLogin.php
@@ -1,13 +1,18 @@
 <?php
-    #include("..config.php");
-    $db = mysqli_real_escape_string("localhost","root","","Airline_test");
+    #include("admin/config/config.php");
+    session_start();
+    $db = mysqli_connect("localhost","root","","airline");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MYSQL: ".mysqli_connect_error();
+    } 
     $email = mysqli_real_escape_string($db,$_POST['email']);
-    $password = mysqli_real_connect($db,$_POST['password']);
-    if(isset($_POST['email']) && isset($_POST['password'])){
-           if($email != NULL && $password != NULL)
-           {
-              echo "Success ".$email." ".$password;
-           }
-        
-    };
+    $password = mysqli_real_escape_string($db,$_POST['password']);
+    
+    $query = "SELECT Email,FirstName FROM Member WHERE Email = '$email' AND Password = '$password'";
+    $result = mysqli_query($db,$query);
+    if($row = mysqli_fetch_array($result)) {
+        $_SESSION['MEmail'] = $row['Email'];
+        $_SESSION['MName'] = $row['FirstName'];
+        header('location: index.php');
+    }
 ?>
