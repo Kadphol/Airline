c373383ac173faa1bf161f14718a15e314b8a022
octobrekids <chnrd.k@gmail.com>
2019-05-28 03:39:51 +0700

    AddOn nearly complete lastest

--- AddOn.php
+++ AddOn.php
@@ -1,6 +1,9 @@
 <!-- import font roboto -->
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
+<!-- import style -->
+<link rel="stylesheet" type="text/css" href="DefaultStyle.css">
+
 <!-- font awesome -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
@@ -23,11 +26,12 @@
 <!--LOGIN PHP-->
 <?php
     session_start();
-    #include("config/config.php");
+    $connection = mysqli_connect("localhost", "root", "", "airline");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
 
     if(isset($_POST['login'])) {
-        $connection = mysqli_connect("localhost", "root", "", "airline");
-
         $email = $connection->real_escape_string($_POST['email']);
         $password = $connection->real_escape_string($_POST['password']);
         
@@ -43,6 +47,29 @@
             exit('please check your input');
         }
     }
+          $_SESSION['FlightID'] = $_GET['FlightID'];
+
+          #BOOKING GET SESSION
+          $dAirportID = $_SESSION['dAirportID'];
+          $aAirtportID = $_SESSION['aAirportID'];
+          $DepartureDate = $_SESSION['DepartureDate'];
+          $ReturnDate = $_SESSION['ReturnDate'];
+          $NumberOfPassenger = $_SESSION['NumberOfPassenger'];
+          $Class = $_SESSION['Class'];
+
+          #QUERY ADDON
+          $query = mysqli_query($connection,"SELECT * FROM AddOn WHERE AddOnClass = '$Class'");
+          while ($result = mysqli_fetch_array($query)) {
+          $AddOnID[] = $result['AddOnID'];
+          $AddOnClass[] = $result['AddOnClass'];
+          $AddOnType[] = $result['AddOnType'];
+          $AddOnPrice[] = $result['AddOnPrice'];
+          $AddOnDescription[] = $result['AddOnDescription'];
+          }
+
+
+
+
 ?>
 
 <html>
@@ -115,25 +142,42 @@
         <?php }
         ?>
 
-         <!--ADD ON-->
-         <div class="wrapper">
-  <div class="container">
-  <h1>jQuery Seat Charts Plugin Demo</h1>
-    <div id="seat-map">
-      <div class="front-indicator">Front</div>
-    </div>
-    <div class="booking-details">
-      <h2>Booking Details</h2>
-      <h3> Selected Seats (<span id="counter">0</span>):</h3>
-      <ul id="selected-seats">
-      </ul>
-      Total: <b>$<span id="total">0</span></b>
-      <button class="checkout-button">Checkout &raquo;</button>
-      <div id="legend"></div>
-    </div>
-  </div>
-</div>
-         
+        <!--ADD ON-->
+        <div class="card-container col-md-8" style="top:10%;">
+          <div class="card-body">
+            <h2><b>Select Add-On</b></h2>
+            <hr>
+            <div class="row d-flex justify-content-center">
+              <table>
+                <tr>
+                <?php
+                  for ($i = 0 ; $i < sizeof($AddOnClass); $i++) { ?>
+                  <td style="padding-right:15px;">
+                  <div class="card" style="max-width: 18rem;">
+                  <div class="card-header bg-dark text-white text-center"><?php echo $AddOnClass[$i]."  ".$AddOnType[$i] ?></div>
+                    <div class="card-body bg-light">
+                      <?php 
+                        $SplitDescription = explode(',',$AddOnDescription[$i]);
+                       
+                        echo $SplitDescription[0].'<br>'.'<br>'.
+                             $SplitDescription[1].'<br>'.'<br>'.
+                             $SplitDescription[2].'<br>'.'<br>'.
+                             $SplitDescription[3].'</br>'.'<br>';
+                      ?>
+
+                <?php $linkAddress = "SelectSeat.php?AddOnType=".$AddOnType[$i];?>
+                <?php echo "<a href='$linkAddress' class='btn btn-primary'><b>Select</b></a>"?>
+
+                    </div>
+                  </div>
+                </div>
+                </td>
+                  <?php } ?>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
     </body>
 </html>
 
@@ -166,113 +210,3 @@
         });
     });
 </script>
-
-<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script> 
-<script src="jquery.seat-charts.js"></script> 
-<script>
-			var firstSeatLabel = 1;
-		
-			$(document).ready(function() {
-				var $cart = $('#selected-seats'),
-					$counter = $('#counter'),
-					$total = $('#total'),
-					sc = $('#seat-map').seatCharts({
-					map: [
-						'ff_ff',
-						'ff_ff',
-						'ee_ee',
-						'ee_ee',
-						'ee___',
-						'ee_ee',
-						'ee_ee',
-						'ee_ee',
-						'eeeee',
-					],
-					seats: {
-						f: {
-							price   : 100,
-							classes : 'first-class', //your custom CSS class
-							category: 'First Class'
-						},
-						e: {
-							price   : 40,
-							classes : 'economy-class', //your custom CSS class
-							category: 'Economy Class'
-						}					
-					
-					},
-					naming : {
-						top : false,
-						getLabel : function (character, row, column) {
-							return firstSeatLabel++;
-						},
-					},
-					legend : {
-						node : $('#legend'),
-					    items : [
-							[ 'f', 'available',   'First Class' ],
-							[ 'e', 'available',   'Economy Class'],
-							[ 'f', 'unavailable', 'Already Booked']
-					    ]					
-					},
-					click: function () {
-						if (this.status() == 'available') {
-							//let's create a new <li> which we'll add to the cart items
-							$('<li>'+this.data().category+' Seat # '+this.settings.label+': <b>$'+this.data().price+'</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
-								.attr('id', 'cart-item-'+this.settings.id)
-								.data('seatId', this.settings.id)
-								.appendTo($cart);
-							
-							/*
-							 * Lets update the counter and total
-							 *
-							 * .find function will not find the current seat, because it will change its stauts only after return
-							 * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
-							 */
-							$counter.text(sc.find('selected').length+1);
-							$total.text(recalculateTotal(sc)+this.data().price);
-							
-							return 'selected';
-						} else if (this.status() == 'selected') {
-							//update the counter
-							$counter.text(sc.find('selected').length-1);
-							//and total
-							$total.text(recalculateTotal(sc)-this.data().price);
-						
-							//remove the item from our cart
-							$('#cart-item-'+this.settings.id).remove();
-						
-							//seat has been vacated
-							return 'available';
-						} else if (this.status() == 'unavailable') {
-							//seat has been already booked
-							return 'unavailable';
-						} else {
-							return this.style();
-						}
-					}
-				});
-
-				//this will handle "[cancel]" link clicks
-				$('#selected-seats').on('click', '.cancel-cart-item', function () {
-					//let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
-					sc.get($(this).parents('li:first').data('seatId')).click();
-				});
-
-				//let's pretend some seats have already been booked
-				sc.get(['1_2', '4_1', '7_1', '7_2']).status('unavailable');
-		
-		});
-
-		function recalculateTotal(sc) {
-			var total = 0;
-		
-			//basically find every selected seat and sum its price
-			sc.find('selected').each(function () {
-				total += this.data().price;
-			});
-			
-			return total;
-		}
-		
-		</script>
