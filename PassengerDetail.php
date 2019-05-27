8525ab8e94de24d44b45210e53e33295748aa3fc
octobrekids <chnrd.k@gmail.com>
2019-05-28 03:42:02 +0700

    passenger detail lastest nearly complete

--- PassengerDetail.php
+++ PassengerDetail.php
@@ -1,4 +1,3 @@
-
 <!-- import font roboto -->
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
@@ -23,47 +22,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <html>
-    <head>
-         <meta charset="utf-8"> 
-         <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
 
-    <body>
-        <div class="card-container col-md-8">
-            <div class="card-body">
-                <h1><b>Passenger Detail</b></h1>
-                <form>
-                    <p>Adult 1</p>
-                    <hr>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
-                    <div class="form-inline">
-                        <input type="text" class="form-control col-md-5" name="FirstName" placeholder="First Name">
-                        <input type="text" class="form-control col-md-5" name="LastName" placeholder="Last Name"><br><br>
-                    </div>
-                    
-                    <p class="top-form">Date of Birth</p>
-                    <div class="form-inline">
-                        <input type="Date" class="form-control col-md-5" name="DOB">
+<body>
+    <div class="card-container col-md-8">
+        <div class="card-body">
+            <h1><b>Passenger Detail</b></h1>
+            <form>
+                <p>Adult 1</p>
+                <hr>
 
-                        <div class="col-md-6">
-                            <div class="form-inline">
-                                <label class="check-space"><input type="radio" class="form-check-input" name="sex" value="m" checked>Male </label>
-                                <label class="check-space"><input type="radio" class="form-check-input" name="sex" value="f">Female</label>
-                            </div>
+                <div class="form-inline">
+                    <input type="text" class="form-control col-md-5" name="FirstName" placeholder="First Name">
+                    <input type="text" class="form-control col-md-5" name="LastName" placeholder="Last Name"><br><br>
+                </div>
+
+                <p class="top-form">Date of Birth</p>
+                <div class="form-inline">
+                    <input type="Date" class="form-control col-md-5" name="DOB">
+
+                    <div class="col-md-6">
+                        <div class="form-inline">
+                            <label class="check-space"><input type="radio" class="form-check-input" name="sex" value="m" checked>Male </label>
+                            <label class="check-space"><input type="radio" class="form-check-input" name="sex" value="f">Female</label>
                         </div>
                     </div>
-                    <br>
-                </form>
-            </div>
+                </div>
+                <br>
+            </form>
         </div>
+    </div>
 
-        <nav class="navbar navbar-expand-sm bg-white navbar-white fixed-bottom">
-            <ul class="nav navbar-nav navbar-left">
-               <p class="total">Total: 245,234 BAHT</p>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-    	        <input type="submit" class="btn btn-secondary navbar-btn" value="Next">
-            </ul>
-        </nav>
-    </body>
+    <nav class="navbar navbar-expand-sm bg-white navbar-white fixed-bottom">
+        <ul class="nav navbar-nav navbar-left">
+            <p class="total">Total: 245,234 BAHT</p>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <input type="submit" class="btn btn-secondary navbar-btn" value="Next">
+        </ul>
+    </nav>
+</body>
+
 </html>
+
+<?php
+$data = (string)$_GET['data'];
+// echo $data;
+
+$SplitDescription = explode(',',$data);
+                       
+echo $SplitDescription[0].'<br>'.'<br>'.
+     $SplitDescription[1].'<br>'.'<br>';
+
+
+?>
