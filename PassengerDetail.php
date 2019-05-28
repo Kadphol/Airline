ded0aa7030595aa4c585327c3805304675cbdbd7
octobrekids <chnrd.k@gmail.com>
2019-05-28 09:04:28 +0700

    Update Passenger detail

--- PassengerDetail.php
+++ PassengerDetail.php
@@ -82,11 +82,6 @@ $_SESSION['SplitSeat'] = $SplitSeat;
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="Mybooking.php">Manage Booking</a>
-                    </li>
-                </ul>
                 <ul class="navbar-nav ml-auto nav-flex-icons">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
