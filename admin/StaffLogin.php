d5d0be8348c2114e1c35a8997a0b9434d56c9346
octobrekids <chnrd.k@gmail.com>
2019-05-28 11:53:40 +0700

    Stafflogin with css

--- admin/StaffLogin.php
+++ admin/StaffLogin.php
@@ -1,41 +1,61 @@
+<!-- import font roboto -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- import style -->
+<link rel="stylesheet" type="text/css" href="StaffStyle.css">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 <html>
-    <head>
-        <title>Staff login</title>
-        <style>
-            ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                background-color: #333;
-            }
 
-            li {
-                float: left;
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-        </style>
-    </head>
-    <body>
-        <ul>
-            <li>Login</li>
-        </ul>
-        <form id='login' action='StaffLogin_php.php' method='post' accept-charset='UTF-8'>
-            <div class="container">
-                <label for="Email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="Email" required>
+<head>
+    <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
-                <label for="Password"><b>Password</b></label>
-                <input type="Password" placeholder="Enter Password" name="Password" required>
+<body>
+    <div class="row justify-content-center">
+        <div class="card-container col-md-8">
+            <div class="card-body">
 
-                <button type="submit">Login</button>
-                </label>
+                <h1><b>Staff Login</b></h1>
+                <hr>
+                <form id='login' action='StaffLogin_php.php' method='post' accept-charset='UTF-8'>
+                    <div class="form-row justify-content-center">
+                        <div class="form-group col-md-6">
+                            <label for="Email"><b>Email</b></label>
+                            <input type="text" class="form-control" placeholder="Enter Email" name="Email" required>
+                        </div>
+                    </div>
+                    <div class="form-row justify-content-center">
+                        <div class="form-group col-md-6">
+                            <label for="Password"><b>Password</b></label>
+                            <input type="Password" class="form-control" placeholder="Enter Password" name="Password" required>
+                        </div>
+                    </div>
+
+                    <div class="container d-flex justify-content-center">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
             </div>
-        </form>
+            </form>
+        </div>
+    </div>
+    </div>
+</body>
 
-    </body>
 </html>
