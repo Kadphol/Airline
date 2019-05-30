609fc150b9380f0cb5a9357b4b55cd4dc4c417e9
octobrekids <chnrd.k@gmail.com>
2019-05-30 10:49:27 +0700

    AddFlight with css

--- admin/AddFlight.php
+++ admin/AddFlight.php
@@ -17,48 +17,108 @@ while ($result2 = mysqli_fetch_array($query2)) {
 
 ?>
 
-<html>
+<!-- import font roboto -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
-<head>
-</head>
+<!-- import style -->
+<link rel="stylesheet" type="text/css" href="StaffStyle.css">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<!DOCTYPE html>
+    <head>
+        <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
 
 <body>
+    <div class="row d-flex justify-content-center " style="margin-bottom:10%">
+        <div class="card-container col-md-8">
+            <div class="card-body">
     <form action="AddFlight_php.php" method="post">
-        <h1>Add Flight</h1>
-        <p>
-            RouteID 
-            <select name="RouteID">
+        <h1><b>Add Flight</b></h1>
+        <div class="form-group">
+            <label>RouteID </label>
+            <select name="RouteID" class="form-control  col-md-5">
                 <option value="" Selected>--RouteID--</option>
                 <?php
                 for ($i = 0; $i < sizeof($RouteID); $i++) { ?>
                     <option> <?php echo $RouteID[$i]." ".$Origin[$i]."-".$Destination[$i] ?> </option>
                 <?php } ?>
-            </select><br><br>
-        </p>
+            </select>
+                </div>
         <hr>
         <p>
-            <!-- FlightID <input type="text" name="FlightID"> Auto Increment -->
-            Departure Time <input type="datetime-local" name="Departure">
-            Arrival Time <input type="datetime-local" name="Arrival"><br><br>
-            AirplaneID 
-            <select name="AirplaneID">
-                <option value="" Selected>--AirplaneID--</option>
-                <?php
-                for ($i = 0; $i < sizeof($AirplaneID); $i++) { ?>
-                    <option> <?php echo $AirplaneID[$i]?> </option>
-                <?php } ?>
-            </select><br><br>
-            Gate <input type="text" name="Gate"><br><br>
-            Status
-            <select name="Status">
+            <div class="form-row justify-content-left">
+                <div class="form-group col-md-5">
+                <!-- FlightID <input type="text" name="FlightID"> Auto Increment -->
+                    <label>Departure Time</label> <input type="datetime-local" name="Departure" class="mx-1 form-control">
+                    <label>Arrival Time</label> <input type="datetime-local" name="Arrival" class="mx-1 form-control">
+                </div>
+            </div>
+            <div class="form-row justify-content-left">
+                <div class="form-group col-md-5">
+                <label>AirplaneID</label>
+                <select name="AirplaneID" class="form-control mx-1">
+                    <option value="" Selected>--AirplaneID--</option>
+                    <?php
+                    for ($i = 0; $i < sizeof($AirplaneID); $i++) { ?>
+                        <option> <?php echo $AirplaneID[$i]?> </option>
+                    <?php } ?>
+                </select>
+               </div>
+            </div>
+
+            <div class="form-row justify-content-left">
+                <div class="form-group col-md-5">
+                <label>Gate</label>
+                 <input type="text" name="Gate" class="form-control mx-1">
+                </div>
+            </div>
+
+            <div class="form-row justify-content-left">
+                <div class="form-group col-md-5">
+                <label>Status</label>
+            <select name="Status" class="form-control mx-1">
                 <option value="" Selected>--Status--</option>
                 <option value="n">Not Active</option>
                 <option value="a">Active</option>
-            </select><br><br>
-            Price <input type="text" name="Price">
+            </select>
+
+                    </div>
+                </div>
+            
+            <div class="form-row justify-content-left">
+                <div class="form-group col-md-5">
+                <label>Price</label>
+                 <input type="text" name="Price" class="form-control mx-1">
+                    </div>
+                    </div>
+           
         </p>
-        <input type="submit" value="Add Flight">
+        <div class="container d-flex justify-content-center">
+        <input type="submit" value="Add Flight" class="btn btn-primary">
+                    </div>
     </form>
+
+                </div>
+                </div>
+                </div>
+
 </body>
 
 </html>
