7bc45cc38457ac6bef42f6e582c46b3d287a5ecc
octobrekids <chnrd.k@gmail.com>
2019-05-30 10:38:24 +0700

    EditRoute with css

--- admin/EditRoute.php
+++ admin/EditRoute.php
@@ -20,35 +20,76 @@
     }
     error_reporting(~E_NOTICE);
 ?>
-<html>
+
+<!-- import font roboto -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+<!-- import style -->
+<link rel="stylesheet" type="text/css" href="StaffStyle.css">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<!DOCTYPE html>
+    <head>
+        <title>Peaktime Report</title>
+        <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
     <body>
+    <div class="row d-flex justify-content-center">
+            <div class="card-container col-md-8">
+                <div class="card-body">
+
+
         <h1>RouteID : <?php echo $RouteID?></h1>
         <hr>
         <p>
-            Origin: <?php echo $Origin?><br>
-            Destination: <?php echo $Destination?><br>
-            Miles: <?php echo $Miles?><br>
+        <b>Origin:</b> <?php echo $Origin?><br>
+            <b>Destination:</b> <?php echo $Destination?><br>
+            <b>Miles:</b> <?php echo $Miles?><br>
         </p>
-    </body>
+ 
+
     <hr>
     <form action="EditRoute_php.php" method="post">
-        Origin
-        <select name="Origin">
+    <b>Origin</b>
+        <select name="Origin" class="form-control col-md-5">
                 <option value="" Selected>--Origin--</option>
                 <?php
                 for ($i = 0; $i < sizeof($AllAirportID); $i++) { ?>
                     <option value="<?php echo $AllAirportID[$i]; ?>"> <?php echo $AllAirportID[$i] ?> </option>
                 <?php } ?>
         </select><br>
-        Destination
-        <select name="Destination">
+        <b>Destination</b>
+        <select name="Destination" class="form-control col-md-5">
                 <option value="" Selected>--Destination--</option>
                 <?php
                 for ($i = 0; $i < sizeof($AllAirportID); $i++) { ?>
                     <option value="<?php echo $AllAirportID[$i]; ?>"> <?php echo $AllAirportID[$i] ?> </option>
                 <?php } ?>
         </select><br>
-        Miles <input type="text" name="Miles">
-        <button type="Submit" name="Edit" value="<?php echo $RouteID ?>" >Edit</button><br>
+        <div class="form-row my-2">
+        <label><b>Miles</b></label> <input type="text" name="Miles" class="form-control col-md-5 mx-2">
+        <button type="Submit" class="btn btn-primary" name="Edit" value="<?php echo $RouteID ?>" >Edit</button>
+        </div><br>
     </form>
+    </div>
+            </div>
+        </div>
+        </body>
 </html>
